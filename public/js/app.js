fc0528caeb7a7f72d8581e08e45a899d6292dc94
Andrew Crawford <acrawford@maiatechnology.com.au>
2021-01-18 11:51:52 +1100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = "Loading"
 
     if (location) {
-        fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        fetch('/weather?address=' + location).then((response) => {
             response.json().then((data) => {
                 if (data.error) {
                     messageTwo.textContent = data.error
